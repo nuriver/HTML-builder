250a1308d24fd277546d413f31a8cd07ec4bceaf
Aleksei Golubkov <nuriver@gmail.com>
2024-01-19 20:43:28 +0100

    feat: finish 04-copy-directory task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+const destDir = path.join(__dirname, "files-copy");
+const sourceDir = path.join(__dirname, "files");
+
+
+fs.mkdir(destDir, { recursive: true }, (err) => {
+  if (err) {
+    console.log(err)
+  }
+});
+
+fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      fs.copyFile(path.join(__dirname, "files", `${file.name}`), path.join(__dirname, "files-copy", `${file.name}`), (err) => {
+        if (err) console.log(err);
+      })
+    })
+}
+});
