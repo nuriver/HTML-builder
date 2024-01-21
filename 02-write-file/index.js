55a0bce69b963f3152d192f216a9b7233f64ffe1
Aleksei Golubkov <nuriver@gmail.com>
2024-01-21 23:01:41 +0100

    fix: add functionality to compare directories in 04-copy-directory task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,6 +2,7 @@ const { stdin, stdout } = process;
 const fs = require('fs');
 const path = require('path');
 const output = fs.createWriteStream(path.join(__dirname, 'output.txt'));
+
 stdout.write('Hello, Stranger! Please, enter whatever you want.\n');
 stdin.on('data', (data) => {
   if (data.toString().trim() === 'exit') {
@@ -13,6 +14,6 @@ stdin.on('data', (data) => {
 process.on('SIGINT', farewell);
 
 function farewell() {
-  console.log('Good luck, Stranger!');
+  console.log('Goodbye, Stranger!');
   process.exit();
 }
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,42 @@
+const { rejects } = require('assert');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 const destDir = path.join(__dirname, 'files-copy');
 const sourceDir = path.join(__dirname, 'files');
 
-fs.mkdir(destDir, { recursive: true }, (err) => {
+function createDir(dir) {
+fs.mkdir(dir, { recursive: true }, (err) => {
   if (err) {
     console.log(err);
+  }
+});
+}
+
+async function isSameFiles() {
+  try {
+    const sourceFiles = await fsPromises.readdir(sourceDir);
+    const destFiles = await fsPromises.readdir(destDir);
+    destFiles.forEach((file) => {
+      if (!sourceFiles.includes(file)) {
+        fs.unlink(path.join(destDir, file), (err) => {
+          console.log(err);
+        })
+      }
+    })
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
+
+fs.access(destDir, err => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      createDir(destDir);
+    }
+  } else {
+    isSameFiles();
   }
 });
 
@@ -24,3 +55,4 @@ fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
     });
   }
 });
+
